a080c28a9a8f8b157f7a525ee626544f7c8f5899
Aida Bogatu <aidaraisa@gmail.com>
2025-09-09 14:32:42 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 echo "Hello World"
 echo "This is running as part of a harness pipeline"
 echo "This is NOT running as part of a harness pipeline"
+echo "This is running as part of a harness pipeline"
